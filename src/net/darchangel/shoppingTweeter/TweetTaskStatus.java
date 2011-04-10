d62b4bfb89d712f00e366e7321a01675b70dfb98
darch <blueberrycandle@gmail.com>
2011-04-10 19:48:12 +0900

    change tweet thread. implements progress icon.

---
+++ src/net/darchangel/shoppingTweeter/TweetTaskStatus.java
@@ -0,0 +1,37 @@
+package net.darchangel.shoppingTweeter;
+
+import twitter4j.Status;
+
+public class TweetTaskStatus {
+
+	private Status tweetStatus = null;
+	private Exception exception = null;
+
+	public TweetTaskStatus() {
+		// do nothing
+	}
+
+	public boolean isSuccess() {
+		if (tweetStatus != null && exception == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void setTweetStatus(Status status) {
+		tweetStatus = status;
+	}
+
+	public Status getTweetStatus() {
+		return tweetStatus;
+	}
+
+	public void setException(Exception e) {
+		exception = e;
+	}
+
+	public Exception getException() {
+		return exception;
+	}
+}
